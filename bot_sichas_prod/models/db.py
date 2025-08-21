6a76663d85b89c9633c993eafb64196592078eeb
dudicoffnet <168195277+dudicoffnet@users.noreply.github.com>
2025-08-21 10:37:30 +0300

    3

--- bot_sichas_prod/models/db.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
-from sqlalchemy.orm import declarative_base, sessionmaker
-import os
-
-DATABASE_URL = os.getenv("DATABASE_URL")
-
-engine = create_async_engine(DATABASE_URL, echo=False)
-SessionLocal = sessionmaker(engine, expire_on_commit=False, class_=AsyncSession)
-Base = declarative_base()
